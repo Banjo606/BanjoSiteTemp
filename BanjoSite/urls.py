ba89dd9eae21e1e9138e562d1d86af47a4f71eb7
Banjo <banjo606.dev@gmail.com>
2023-07-24 01:23:20 +0800

    feat: add 539 Hello World
    建立 539 初始頁面

--- BanjoSite/urls.py
+++ BanjoSite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # 引用include函式
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('539/', include('Lottery_539.urls')),
 ]
